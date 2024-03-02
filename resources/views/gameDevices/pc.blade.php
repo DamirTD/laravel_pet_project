798ce95f1d91c6bd047c75adb9bf0191a354a87a
Damir Toriya <61018701+DamirTD@users.noreply.github.com>
2024-03-02 22:24:23 +0600

    book blocks added

---
+++ resources/views/gameDevices/pc.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    @for ($i = 1; $i <= $totalPCs; $i++)
+        <div class="playstation-square" style="border: 1px solid black; width: 50px; height: 50px; float: left; margin-right: 10px; margin-bottom: 10px;">
+            <button style="width: 50px; height: 50px;" data-id="{{$i}}" @if($i > $availablePCs) disabled @endif>{{$i}}</button>
+        </div>
+        @if ($i % 3 == 0)
+            <div style="clear: both;"></div>
+        @endif
+    @endfor
+</div>
+</body>
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,28 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Register</title>
+    <title>Home</title>
 </head>
 <body>
-
-
     @auth
     <p>You are logged in!</p>
     <form action="/logout" method="post">
         @csrf
         <button>Log out</button>
     </form>
-    @else
+    <p>Choose what you want</p>
+    <form method="get" action="{{ route('playstation') }}">
+        <button type="submit">PlayStation</button>
+    </form>
 
+    <form method="get" action="{{ route('game_pc') }}">
+        <button type="submit">Game PC</button>
+    </form>
+
+    <form method="get" action="{{ route('pc') }}">
+        <button type="submit">PC</button>
+    </form>
+    @else
         <div style="border: 3px solid black;">
             <h2>Register</h2>
             <form action="/register" method="post">
@@ -33,7 +42,6 @@
                 <button>Register</button>
             </form>
         </div>
-
         <div style="border: 3px solid black;">
             <h2>Login</h2>
             <form action="/login" method="post">
@@ -47,7 +55,6 @@
                 <button>Login</button>
             </form>
         </div>
-
     @endauth
 </body>
 </html>
